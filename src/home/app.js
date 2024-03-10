3de99bf8ad3e4df93cf53ff63f5ebf026ee13fed
Raphael GEORGET <raphael.georgetpro@orange.fr>
2024-03-10 20:20:40 +0100

    [ADD] images limk API

--- src/home/app.js
+++ src/home/app.js
@@ -1,31 +1,58 @@
 const url = "https://api.kedufront.juniortaker.com/";
 
-function get_data() {
-
-    let elements = -1;
-    const api_data = axios.get(url + "item/")
+function getPicturefromID(id) {
+    axios
+        .get(url + `item/picture/${id}`)
         .then(response => {
-            console.log(response);
-            if (response.status === 200) {
-                console.log("API works !");
-                return response.data;
+            if (response.status !== 200) {
+                console.error("Error picture !");
             } else {
-                console.error("HTTP GET error");
-                elements = -1;
+                const pictureUrl = URL.createObjectURL(response.data);
+                const pictureContainer = document.getElementById(`picture-container-${id}`);
+                if (pictureContainer) {
+                    const pictureElement = document.createElement('img');
+                    pictureElement.src = pictureUrl;
+                    pictureElement.alt = 'Item Image';
+                    pictureContainer.appendChild(pictureElement);
+                } else {
+                    console.error(`Image container for item ${id} not found`);
+                }
             }
-        })
-    return elements;
+        });
 }
 
-function create_product() {
-    let product = get_data();
 
-    if (product) {
-        console.log("test");
+async function getItems() {
+    try {
+        const response = await axios.get(url +'item/');
+        console.log(response);
+        return response.data;
+    } catch (error) {
+        console.error(error);
     }
-    for (let x = 0; x < product.length; x++) {
-        console.log(product[x]);
-    }
+}
+
+async function create_product() {
+    let item_array = await getItems();
+    const container = document.getElementById('items_emplacement');
+
+    item_array.forEach(item => {
+        const img_URL = url + `item/picture/${item._id}`;
+        axios
+            .get(url)
+            .then(() => {});
+        const new_div = document.createElement('div');
+        new_div.className = 'item';
+        new_div.innerHTML = `
+        <a href="./products/${item.name}.html">
+            <div id="image-container-${item.image}"></div>
+            <h2>${item.name}</h2>
+            <img src="${img_URL}" alt="item_image">
+            <p>Prix: ${item.price}</p>
+          </a>
+        `;
+        container.appendChild(new_div);
+    });
 }
 
 create_product();
